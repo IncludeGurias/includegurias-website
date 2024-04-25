3e8db8640b2ebf3275495494d37684d74f541d5f
Lucas Diniz <63087780+LucasHenriqueDiniz@users.noreply.github.com>
2024-04-25 03:50:35 -0300

    removed test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,15 @@
 import { expect, test } from "@playwright/test"
 
-test("has title", async ({ page }) => {
-  await page.goto("./")
+// test("has title", async ({ page }) => {
+//   await page.goto("./")
 
-  await expect(page).toHaveTitle("Include Gurias")
-})
+//   await expect(page).toHaveTitle("Include Gurias")
+// })
+
+// test("has news", async ({ page }) => {
+//   await page.goto("./")
+
+//   const news = await page.$$(".news-item")
+
+//   expect(news).toHaveLength(4)
+// })
